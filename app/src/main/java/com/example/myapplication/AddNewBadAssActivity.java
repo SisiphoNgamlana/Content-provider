49aa94c0d92b2ab15d24f094d42fb0ca95a67526
Amandla Ngamlana <amandlangamlana@yahoo.com>
2022-04-14 07:48:50 +0200

    Adding new add new badass activity and hook up new activity

---
+++ app/src/main/java/com/example/myapplication/AddNewBadAssActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewBadAssActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //TODO : Use ConstraintLayout
+        setContentView(R.layout.activity_add_new_bad_ass);
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String BAD_ASS_FRAGMENT_TAG = "BAD_ASS_FRAGMENT_TAG";
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem menuItem) {
+        if (menuItem.getItemId() == R.id.new_badass_menu_item) {
+            Intent intent = new Intent(getApplicationContext(), AddNewBadAssActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
